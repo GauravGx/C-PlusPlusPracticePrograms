d4f0547e520ced38a131e1a1fd2fd253d0e3ed42
Gaurav Gx <Gaurav10intel@gmail.com>
2024-09-20 23:26:22 +0530

    adding all data

---
+++ SizeOfCharacterConstant.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main()
+{
+  char ch= 'A';
+  cout<<"size of char Constant : "<<sizeof(ch);
+  
+  return 0;    
+}
---
+++ SizeOfRealConstant.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float f = 45.23;
+    cout<<"SizeOfRealConstat "<<f;
+   
+    return 0;   
+}
