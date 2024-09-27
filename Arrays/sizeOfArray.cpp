62bc280de8357310455f6129a50d32841a5d3b92
Gaurav Gx <Gaurav10intel@gmail.com>
2024-09-27 22:47:55 +0530

    size of array

---
+++ Arrays/sizeOfArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+  int num[] = {10,20,30,40,50,60};
+  int size = sizeof(num)/sizeof(num[0]);
+  cout<<"size of num array is : "<<size;
+
+}
